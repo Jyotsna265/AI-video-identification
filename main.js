59474b0227a89a258687eb76d4551aabbf90560a
Jyotsna265 <74059430+Jyotsna265@users.noreply.github.com>
2021-07-27 18:55:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objects = [];
+status = "";
 video = "";
 
 function preload()
@@ -15,6 +17,23 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " +objects. length;
+            
+         fill("#2af5d0");
+         percent = floor(objects[i].confidence * 100);
+         text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15 );
+         noFill();
+         stroke("#2af5d0");
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
 }
 
 function start()
@@ -30,4 +49,13 @@ function modelLoded()
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
